22a3bd91dd2547af891f0fba134164e032dd0148
F-KZ <supreme-soul@hotmail.fr>
2024-06-07 15:58:18 +0200

    config route

---
+++ back/routes/formulaire.js
@@ -0,0 +1,42 @@
+import { createTransport } from 'nodemailer';
+import { config } from 'dotenv';
+
+// Charger les variables d'environnement depuis le fichier .env
+config();
+// Configuration de Nodemailer
+const transporter = createTransport({
+    host: 'smtp.office365.com', // Hotmail/Outlook SMTP server
+     port: 587,
+     secure: false, // Use TLS
+     auth: {
+       user: process.env.EMAIL_USER,
+       pass: process.env.EMAIL_PASS
+     },
+     tls: {
+       ciphers: 'SSLv3'
+     },
+     debug: true // Enable debug mode
+   });
+   
+   console.log('Email User:', process.env.EMAIL_USER);
+   console.log('Email Pass:', process.env.EMAIL_PASS ? 'Loaded' : 'Not Loaded');
+   
+   const sendForm = (req, res) => {
+     const { firstName, email, message } = req.body;
+   
+     const mailOptions = {
+       from: process.env.EMAIL_USER,
+       to: `${email}`,
+       subject: 'Nouveau message du formulaire',
+       text: `Nom Complet: ${firstName}\nEmail: ${email}\nMessage: ${message}`
+     };
+   
+     transporter.sendMail(mailOptions, (error, info) => {
+       if (error) {
+         return res.status(500).send(error.toString());
+       }
+       res.status(200).send('Message envoyé avec succès !');
+     });
+   };
+
+   export default sendForm
