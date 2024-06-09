4bd4deea3a9bd847fdd567f9847705296c30b50e
F-KZ <supreme-soul@hotmail.fr>
2024-06-09 12:38:12 +0200

    .

--- back/index.js
+++ back/index.js
@@ -29,5 +29,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/`);
+  console.log(`adresse du serveur http://localhost:${PORT}/`);
 });
